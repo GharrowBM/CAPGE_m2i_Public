80ba9987e048f523f80d535614b7b4b0d542d787
GharrowBM <gharrow.games@gmail.com>
2023-03-27 12:10:22 +0200

    Init bases Docker

---
+++ demo-docker-basics/main.py
@@ -0,0 +1,5 @@
+number_a = int(input("Veuillez entrer un premier nombre: "))
+number_b = int(input("Veuillez entrer un second nombre: "))
+number_c = int(input("Veuillez entrer un second nombre: "))
+
+print(f"{number_a} + {number_b} + {number_c} = {number_b + number_a + number_c}")
